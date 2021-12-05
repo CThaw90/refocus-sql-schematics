d459f922f65b293ccf11ce3bfcceb5dcb0e888ec
CThaw90 <christhaw90@gmail.com>
2021-12-05 12:50:31 -0500

    Refactoring logging data and shutting down mysql container after dbcc script finishes

--- dbcc.sh
+++ dbcc.sh
@@ -1,21 +1,27 @@
+end () {
+	mysqladmin shutdown
+	exit $1
+}
+
 if [ -z "$CHANGES_FILE" ]; then
 	echo "Missing environment variable CHANGES_FILE"
-	exit 1
+	mysqladmin shutdown
+	end "1"
 elif [ -z "$DB_HOST" ]; then
 	echo "Missing environment variable DB_HOST"
-	exit 1
+	end "1"
 elif [ -z "$DB_USER" ]; then
 	echo "Missing environment variable DB_USER"
-	exit 1
+	end "1"
 elif [ -z "$DB_PASS" ]; then
 	echo "Missing environment variable DB_PASS"
-	exit 1
+	end "1"
 elif [ -z "$CHANGES_DB" ]; then
 	echo "Missing environment variable CHANGES_DB"
-	exit 1
+	end "1"
 elif [ -z "$APP_DB" ]; then
 	echo "Missing environment variable APP_DB"
-	exit 1
+	end "1"
 fi
 
 if [ -z "$CHANGES_HOME" ]; then
@@ -24,7 +30,7 @@ fi
 
 if [ ! -f $CHANGES_HOME/$CHANGES_FILE ]; then
 	echo "Could not find changes file at $CHANGES_HOME/$CHANGES_FILE"
-	exit 1
+	end "1"
 fi
 
 export MYSQL_PWD=$DB_PASS
@@ -32,13 +38,12 @@ while read CHANGE; do
 	CHANGES_FILE_PATH="$CHANGES_HOME$CHANGE"
 	echo $CHANGES_FILE_PATH
 	if [ -f $CHANGES_FILE_PATH ]; then
-		echo $MYSQL_PWD
 		applied=`mysql -h "$DB_HOST" -u"$DB_USER" changesets -s -N -e "SELECT applied FROM $APP_DB WHERE file_path='$CHANGE' LIMIT 1"`
 		echo $applied
 		if [ ${#applied} -eq 1 ]; then
-			echo "There is a record available"
+			echo "$CHANGE has already been applied"
 		else
-			echo "There is no record available"
+			echo "Applying changeset - $CHANGE"
 			mysql -h "$DB_HOST" -u"$DB_USER" "$APP_DB" -s -N -e "`cat $CHANGES_FILE_PATH`"
 			mysql -h "$DB_HOST" -u"$DB_USER" changesets -s -N -e "INSERT INTO $APP_DB (file_path) VALUES ('$CHANGE')"
 		fi
@@ -47,3 +52,5 @@ while read CHANGE; do
 	fi
 
 done < $CHANGES_HOME/$CHANGES_FILE
+
+end "0"
