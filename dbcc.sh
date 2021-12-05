faa1b049fe073f8fd993c4d51532e0a8c5b098a4
CThaw90 <christhaw90@gmail.com>
2021-12-05 12:28:22 -0500

    Correcting file path for changes.txt logic

--- dbcc.sh
+++ dbcc.sh
@@ -18,20 +18,20 @@ elif [ -z "$APP_DB" ]; then
 	exit 1
 fi
 
-if [ ! -f $CHANGES_FILE ]; then
-	echo "Could not find changes file at $CHANGES_FILE"
-	exit 1
-fi
-
 if [ -z "$CHANGES_HOME" ]; then
 	CHANGES_HOME=.
 fi
 
+if [ ! -f $CHANGES_HOME/$CHANGES_FILE ]; then
+	echo "Could not find changes file at $CHANGES_HOME/$CHANGES_FILE"
+	exit 1
+fi
+
 export MYSQL_PWD=$DB_PASS
 while read CHANGE; do
-	CHANGE_FILE_PATH="$CHANGES_HOME$CHANGE"
-	echo $CHANGE_FILE_PATH
-	if [ -f $CHANGE_FILE_PATH ]; then
+	CHANGES_FILE_PATH="$CHANGES_HOME$CHANGE"
+	echo $CHANGES_FILE_PATH
+	if [ -f $CHANGES_FILE_PATH ]; then
 		echo $MYSQL_PWD
 		applied=`mysql -h "$DB_HOST" -u"$DB_USER" changesets -s -N -e "SELECT applied FROM $APP_DB WHERE file_path='$CHANGE' LIMIT 1"`
 		echo $applied
@@ -39,11 +39,11 @@ while read CHANGE; do
 			echo "There is a record available"
 		else
 			echo "There is no record available"
-			mysql -h "$DB_HOST" -u"$DB_USER" "$APP_DB" -s -N -e "`cat $CHANGE_FILE_PATH`"
+			mysql -h "$DB_HOST" -u"$DB_USER" "$APP_DB" -s -N -e "`cat $CHANGES_FILE_PATH`"
 			mysql -h "$DB_HOST" -u"$DB_USER" changesets -s -N -e "INSERT INTO $APP_DB (file_path) VALUES ('$CHANGE')"
 		fi
 	else
-		echo "File $CHANGE_FILE_PATH not found"
+		echo "File $CHANGES_FILE_PATH not found"
 	fi
 
 done < $CHANGES_HOME/$CHANGES_FILE
