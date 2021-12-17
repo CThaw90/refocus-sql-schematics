27b8c898e1be37c0d9b941e355d1a122cb274698
CThaw90 <christhaw90@gmail.com>
2021-12-17 12:05:58 -0500

    Changed REMOVE to DROP for removing coordinate columns in county_location_data table

--- county_location_data/remove_longitude_latitude_columns.sql
+++ county_location_data/remove_longitude_latitude_columns.sql
@@ -1,3 +1,3 @@
 -- Remove coordinate columns longitude and latitude
-ALTER TABLE county_location_data REMOVE COLUMN longitude;
-ALTER TABLE county_location_data REMOVE COLUMN latitude;
+ALTER TABLE county_location_data DROP COLUMN longitude;
+ALTER TABLE county_location_data DROP COLUMN latitude;
