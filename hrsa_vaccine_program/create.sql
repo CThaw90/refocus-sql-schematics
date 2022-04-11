0e904724db5f48c59e019b548df4614be959befc
CThaw90 <christhaw90@gmail.com>
2022-04-10 23:25:48 -0400

    Fix table schematics for hrsa_vaccine_program

--- hrsa_vaccine_program/create.sql
+++ hrsa_vaccine_program/create.sql
@@ -1,6 +1,6 @@
 -- Create schematics for the hrsa_vaccine_program table
 CREATE TABLE hrsa_vaccine_program (
-	id INT NOT NULL PRIMARY AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	health_center TINYTEXT NOT NULL,
 	city VARCHAR(50) NOT NULL,
 	county VARCHAR(50) NOT NULL,
