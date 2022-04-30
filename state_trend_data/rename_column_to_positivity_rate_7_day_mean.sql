1109683fb8d1c988966b1661108fca3ed139e7c1
CThaw90 <christhaw90@gmail.com>
2022-04-30 11:59:33 -0400

    Renaming column positivity_rate_7_day_plus_mean to positivity_rate_14_day_mean in the state_trend_data table

---
+++ state_trend_data/rename_column_to_positivity_rate_7_day_mean.sql
@@ -0,0 +1,2 @@
+-- Modify column positivity_rate_7_day_plus_mean to positivity_rate_14_day_mean
+ALTER TABLE state_trend_data RENAMT COLUMN positivity_rate_7_day_plus_mean TO positivity_rate_14_day_mean;
